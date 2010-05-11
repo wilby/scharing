8e43cf46c6c3d029567e93f48b2af9f880df3e27
Wilby Jackson <wilby@contempt.(none)>
2010-05-11 18:50:12 -0400

    Added Intents file

---
+++ src/net/wcjj/scharing/ScharingIntents.java
@@ -0,0 +1,8 @@
+package net.wcjj.scharing;
+
+public class ScharingIntents {
+
+	public final static String SHOW_SHARING_ALERTS = "net.wcjj.scharing.SHOW_SHARING_ALERTS";
+	public final static String HIDE_SHARING_ALERTS = "net.wcjj.scharing.HIDE_SHARING_ALERTS";
+	public final static String REQUEST_SHOW_ALERTS_STATE = "net.wcjj.sharing.REQUEST_SHOW_ALERTS_STATE";
+}
